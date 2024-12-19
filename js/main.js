69bea179367a29d46f644bd65da10ce36144fae7
cayesito <cyestem@student.ifp.es>
2024-12-19 12:47:50 +0100

    added email sender

--- js/main.js
+++ js/main.js
@@ -3,3 +3,31 @@ window.addEventListener("scroll", function () {
     let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
     header.style.opacity = 1 - scrollTop / 500;
 });
+
+window.addEventListener("scroll", function () {
+    let footer = document.getElementById("footer");
+    let scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    footer.style.opacity = 1 + scrollTop * 500;
+});
+
+const btn = document.getElementById('button');
+
+document.getElementById('form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'Sending...';
+
+   const serviceID = 'default_service';
+   const templateID = 'template_r2eagi7';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Send Email';
+      alert('Sent!');
+    }, (err) => {
+      btn.value = 'Send Email';
+      alert(JSON.stringify(err));
+    });
+});
+
